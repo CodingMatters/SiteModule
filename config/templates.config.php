2dc16cbd802bbdf0e14534b54e9fd57ce82ab9cf
gabbydgab <gamba@gabbydgab.com>
2016-06-10 13:38:20 +0800

    fixed configuration

--- config/templates.config.php
+++ config/templates.config.php
@@ -10,16 +10,16 @@ return [
         'exception_template'        => 'error/index',
         'layout'                    => 'site/layout',
         'template_map' => [
-            'layout/layout'             => __DIR__ . '/../view/layout/site.phtml',
-            'site/layout'               => __DIR__ . '/../view/layout/site.phtml',
-            'template/footer'           => __DIR__ . '/../view/layout/partial/footer.phtml',
-            'template/navigation'       => __DIR__ . '/../view/layout/partial/navigation.phtml',
-            'template/breadcrumbs'      => __DIR__ . '/../view/layout/partial/breadcrumbs.phtml',            
-            'error/404'                 => __DIR__ . '/../view/error/404.phtml',
-            'error/index'               => __DIR__ . '/../view/error/index.phtml',
+            'layout/layout'             => __DIR__ . '/../templates/layout/site.phtml',
+            'site/layout'               => __DIR__ . '/../templates/layout/site.phtml',
+            'template/footer'           => __DIR__ . '/../templates/layout/partial/footer.phtml',
+            'template/navigation'       => __DIR__ . '/../templates/layout/partial/navigation.phtml',
+            'template/breadcrumbs'      => __DIR__ . '/../templates/layout/partial/breadcrumbs.phtml',            
+            'error/404'                 => __DIR__ . '/../templates/error/404.phtml',
+            'error/index'               => __DIR__ . '/../templates/error/index.phtml',
         ],
         'template_path_stack' => [
-            'site' => __DIR__ . '/../view',
+            'site' => __DIR__ . '/../templates',
         ],
     ],
 
@@ -32,16 +32,16 @@ return [
         'exception_template'        => 'error/index',
         'layout'                    => "default/layout",
         'map'       => [
-            'default/layout'            => __DIR__ . '/../view/layout/default.phtml',
-            'site/layout'               => __DIR__ . '/../view/layout/site.phtml',
-            'template/footer'           => __DIR__ . '/../view/layout/partial/footer.phtml',
-            'template/navigation'       => __DIR__ . '/../view/layout/partial/navigation.phtml',
-            'template/breadcrumbs'      => __DIR__ . '/../view/layout/partial/breadcrumbs.phtml',            
-            'error/404'                 => __DIR__ . '/../view/error/404.phtml',
-            'error/index'               => __DIR__ . '/../view/error/index.phtml',
+            'default/layout'            => __DIR__ . '/../templates/layout/default.phtml',
+            'site/layout'               => __DIR__ . '/../templates/layout/site.phtml',
+            'template/footer'           => __DIR__ . '/../templates/layout/partial/footer.phtml',
+            'template/navigation'       => __DIR__ . '/../templates/layout/partial/navigation.phtml',
+            'template/breadcrumbs'      => __DIR__ . '/../templates/layout/partial/breadcrumbs.phtml',            
+            'error/404'                 => __DIR__ . '/../templates/error/404.phtml',
+            'error/index'               => __DIR__ . '/../templates/error/index.phtml',
         ],
         'paths'     => [
-            'site'  => [__DIR__ . '/../view']
+            'site'  => [__DIR__ . '/../templates']
         ]
     ],
 
